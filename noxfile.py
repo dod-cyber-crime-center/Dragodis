f22cc01016b35c93171fbd5c8ab5aeaff0376938
DC3-DCCI <12175126+DC3-DCCI@users.noreply.github.com>
2022-02-03 14:32:10 -0500

    Updated noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,35 @@
+"""
+Runs tests and other routines.
+
+Usage:
+  1. Install "nox"
+  2. Run "nox" or "nox -s test"
+"""
 import shutil
 import os
 import nox
 
 
+@nox.session(python="3.8")
+def test(session):
+    """Run pytests"""
+    session.install("-e", ".[testing]")
+    session.run("pytest")
+
+
+@nox.session(python="3.8")
+def build(session):
+    """Build source and wheel distribution"""
+    session.run("python", "setup.py", "sdist")
+    session.run("python", "setup.py", "bdist_wheel")
+
+
 @nox.session
 def doc(session):
     """Builds Sphinx documentation"""
+    session.run("mkdir", "-p", "dist", external=True)
     shutil.rmtree("docs/_build", ignore_errors=True)
-    shutil.rmtree("dist", ignore_errors=True)
-    os.mkdir("dist")
+    shutil.rmtree("dist/docs", ignore_errors=True)
     session.install("sphinx")
     session.install("sphinx-rtd-theme")
     session.install("-e", ".")
@@ -19,4 +40,16 @@ def doc(session):
 
     # Build html site
     session.run("sphinx-build", "docs", "docs/_build/html", "-b", "html")
-    shutil.copytree("docs/_build/html", f"dist/Dragodis_documentation")
+    shutil.copytree("docs/_build/html", f"dist/docs")
+
+
+@nox.session(python=False)
+def release_patch(session):
+    """Generate release patch"""
+    session.run("mkdir", "-p", "dist", external=True)
+    with open("./dist/updates.patch", "w") as out:
+        session.run(
+            "git", "format-patch", "--stdout", "master",
+            external=True,
+            stdout=out
+        )
